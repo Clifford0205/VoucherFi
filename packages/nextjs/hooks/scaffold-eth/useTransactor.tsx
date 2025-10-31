73d6330d078ee8e81e4aecdf9c8f4281de12678c
clifford <ts02690140@gmail.com>
2025-10-31 18:16:44 +0800

    feat(usetransactor): duration: 5000

--- packages/nextjs/hooks/scaffold-eth/useTransactor.tsx
+++ packages/nextjs/hooks/scaffold-eth/useTransactor.tsx
@@ -87,6 +87,7 @@ export const useTransactor = (_walletClient?: WalletClient): TransactionFunc => 
         <TxnNotification message="Transaction completed successfully!" blockExplorerLink={blockExplorerTxURL} />,
         {
           icon: "🎉",
+          duration: 5000,
         },
       );
 
